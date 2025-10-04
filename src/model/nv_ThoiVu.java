76dd5c3b5841290041de76c0d651277d4d694eae
hat262006 <hat262006@gmail.com>
2025-10-05 03:05:36 +0700

    C

--- src/model/nv_ThoiVu.java
+++ src/model/nv_ThoiVu.java
@@ -1,7 +1,9 @@
 package model;
+
 import java.sql.Date;
 import service.ChamCong;
 import model.nhan_vien;
+
 /**
  * @author Hat262006
  */
@@ -10,18 +12,23 @@ public class nv_ThoiVu extends nhan_vien {
     //Contructor 
     public nv_ThoiVu(double luongMotGio, String maNV, String tenNV,
             String gioiTinh, String chucVu, Date ngaySinh, String cccd,
-            Date ngayVaoLam, String trinhDo, String soDienThoai, 
+            Date ngayVaoLam, String trinhDo, String soDienThoai,
             String trangThai, String anh) {
         super(maNV, tenNV, gioiTinh, chucVu, ngaySinh, cccd, ngayVaoLam,
                 trinhDo, soDienThoai, trangThai, anh);
         this.luongMotGio = luongMotGio;
     }
-public double getLuongMotGio() {return luongMotGio;}
-public void setLuongMotGio(double luongMotGio) {this.luongMotGio = luongMotGio;}   
+//Set & get 
+    public double getLuongMotGio() {
+        return luongMotGio;
+    }
+    public void setLuongMotGio(double luongMotGio) {
+        this.luongMotGio = luongMotGio;
+    }
     // Tinh luong
     @Override
     public double tinhLuong(ChamCong cc) {
-        return  (cc.getGioLam() * luongMotGio) + (cc.getGioTangCa()
+        return (cc.getGioLam() * luongMotGio) + (cc.getGioTangCa()
                 * cc.getHsTangCa() * luongMotGio);
     }
 }
